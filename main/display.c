02743448e485f1c96b94785a7bcc3cfed851097f
David Antliff <david@pitchblende.co.nz>
2018-03-26 22:05:03 +1300

    Fix zero pin bit mask when using GPIO > 31.

--- main/display.c
+++ main/display.c
@@ -874,7 +874,7 @@ static void button_task(void * pvParameter)
     gpio_config_t btn_config;
     btn_config.intr_type = GPIO_INTR_ANYEDGE;    // Enable interrupt on both rising and falling edges
     btn_config.mode = GPIO_MODE_INPUT;           // Set as Input
-    btn_config.pin_bit_mask = (1 << CONFIG_DISPLAY_BUTTON_GPIO); // Bitmask
+    btn_config.pin_bit_mask = (UINT64_C(1) << CONFIG_DISPLAY_BUTTON_GPIO); // Bitmask
     btn_config.pull_up_en = GPIO_PULLUP_ENABLE;      // Disable pullup
     btn_config.pull_down_en = GPIO_PULLDOWN_DISABLE; // Disable pulldown
     gpio_config(&btn_config);
